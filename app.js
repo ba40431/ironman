f94cb26af56a38f6429561897a2f778e810f904e
abby.ho <abby.ho@eztable.com>
2023-08-29 16:57:52 +0800

    add ejs

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = require('./routes')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.use(router)
 app.use(express.static('public'))
+app.set('views',__dirname + '/public/views')
+app.set('view engine', 'ejs')
+
+// app.get("/",(req,res)=>{
+//   res.render('book');
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
