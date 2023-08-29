96a2244399879e96c5d08fb518a72c6b8c443c0d
abby.ho <abby.ho@eztable.com>
2023-08-29 16:59:16 +0800

    delete commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = require('./routes')
-const path = require('path')
 const app = express()
 const port = 3000
 
@@ -9,9 +8,6 @@ app.use(express.static('public'))
 app.set('views',__dirname + '/public/views')
 app.set('view engine', 'ejs')
 
-// app.get("/",(req,res)=>{
-//   res.render('book');
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
