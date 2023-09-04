3f71f849b0b8bd023ab189dc42bc956e6f214871
abby <ba40431@gmail.com>
2023-09-04 22:54:47 +0800

    fix-connection-to-promise

--- config/mysqlConnection.js
+++ config/mysqlConnection.js
@@ -1,32 +1,29 @@
-// get the client
-const mysql = require('mysql2')
+async function mysqlConnection() {
+  // get the client
+  const mysql = require('mysql2/promise')
 
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-	port: 3306,
-  user: 'root',
-  database: 'book',
-	password: ''
-})
+  // create the connection to database
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    database: 'book',
+    password: ''
+  })
+  
+  // query database
+  const [rows1, fields1] = await connection.execute('INSERT INTO `booktest` (`bookName`) VALUES ("testBook")')
+  console.log(rows1)
 
-connection.query(
-  'INSERT INTO `booktest` (`bookName`) VALUES ("testBook")',
-  (err, results, fields) => {
-    console.log(results) 
-  }
-)
+   // query database
+   const [rows2, fields2] = await connection.execute('UPDATE `booktest` SET `bookName` = "testBook1" WHERE id = 1')
+   console.log(rows2)
 
-connection.query(
-  'UPDATE `booktest` SET `bookName` = "testBook" WHERE id = 1',
-  (err, results, fields) => {
-    console.log(results)
-  }
-)
+    // query database
+  const [rows3, fields3] = await connection.execute('SELECT * FROM `booktest`')
+  console.log(rows3)
 
-connection.query(
-  'SELECT * FROM `booktest`',
-  (err, results, fields) => {
-    console.log(results)
-  }
-)
+  return 
+}
+
+mysqlConnection()
