36285b92a99ea5a40a9d81553d951036d10f8a12
abby.ho <abby.ho@eztable.com>
2023-09-05 16:15:18 +0800

    add-sqlto-router

---
+++ services/bookServices.js
@@ -0,0 +1,30 @@
+const BookRepository = require('../repositories/BookRepository')
+const bookRepository = new BookRepository()
+
+class BookService {
+  constructor() {
+  }
+
+  async createBook() {
+
+    return 
+  }
+
+  async selectBook() {
+
+    return 
+  }
+
+  async updateBook() {
+
+    return 
+  }
+
+  async deleteBook() {
+
+    return 
+  }
+
+}
+
+module.exports = BookService
