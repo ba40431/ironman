b094c95777a338f93e836e73ddbbabc80e0a7b69
abby.ho <abby.ho@eztable.com>
2023-09-05 18:17:15 +0800

    fix-connect-function

--- config/mysqlConnection.js
+++ config/mysqlConnection.js
@@ -1,34 +1,42 @@
 const mysql = require('mysql2/promise')
 
-class Mysql {
-  constructor() {
-  }
+// class Mysql {
+//   constructor() {
+//   }
 
-  async createConnection() {
-  return await mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    database: 'book',
-    password: ''
-  })
-  }
-}
-
-module.exports = Mysql
-// async function mysqlConnection() {
-//   // get the client
-//   const mysql = require('mysql2/promise')
-
-//   // create the connection to database
-//   const connection = await mysql.query({
+//   async createConnection() {
+//   return await mysql.createConnection({
 //     host: 'localhost',
 //     port: 3306,
 //     user: 'root',
 //     database: 'book',
 //     password: ''
 //   })
-  
+//   }
+// }
+
+// module.exports = Mysql
+
+async function mysqlConnection() {
+  // get the client
+  // const mysql = require('mysql2/promise')
+
+  // create the connection to database
+  const connection = await mysql.query({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    database: 'book',
+    password: ''
+  })
+
+  return connection
+}
+
+module.exports = {
+  mysqlConnection
+}
+
 //   // query database
 //   const [rows1, fields1] = await connection.execute('INSERT INTO `booktest` (`bookName`) VALUES ("testBook")')
 //   console.log(rows1)
