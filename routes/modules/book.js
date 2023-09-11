702763c0a67e35d00af1e280391bbc3845e93056
abby.ho <abby.ho@eztable.com>
2023-09-11 16:31:48 +0800

    add-login-page

--- routes/modules/book.js
+++ routes/modules/book.js
@@ -29,20 +29,12 @@ router.get('/', async (req, res) => {
 
 // define the book route by post method
 router.post('/', async (req, res) => {
-<<<<<<< HEAD
-=======
-  console.log(req.body)
->>>>>>> upstream
   const bookName = req.body.bookName // 設定一個要新增的書名
 
   try {
     // 建立與數據庫的連接
     const connection = await mysqlConnection()
-<<<<<<< HEAD
-    await connection.query(`INSERT INTO booktest (bookName) VALUES (?)`, bookName)
-=======
     await connection.query(`INSERT INTO booktest (bookName) VALUES ('${bookName}')`)
->>>>>>> upstream
 
     // 關閉連接
     connection.end()
@@ -55,15 +47,6 @@ router.post('/', async (req, res) => {
 })
 
 // define the book route by delete method
-<<<<<<< HEAD
-router.delete('/:id', async (req, res) => { // 設定一個要刪除書名的 id
-  const id = req.params.id
-
-  try {
-    // 建立與數據庫的連接
-    const connection = await mysqlConnection()
-    await connection.query(`DELETE FROM booktest  WHERE id = ?`, id)
-=======
 router.delete('/:id', async (req, res) => {
   const id = req.params.id
   
@@ -71,7 +54,6 @@ router.delete('/:id', async (req, res) => {
     // 建立與數據庫的連接
     const connection = await mysqlConnection()
     await connection.query(`DELETE FROM booktest  WHERE id = ${id}`)
->>>>>>> upstream
 
     // 關閉連接
     connection.end()
