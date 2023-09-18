727496a87dd39fafdd543f696bc41e9a57ddc15c
abby <ba40431@gmail.com>
2023-09-18 21:41:20 +0800

    update-index.js

--- routes/modules/index.js
+++ routes/modules/index.js
@@ -6,13 +6,15 @@ const requireLogin = require('../../middleware/auth')
 router.get('/', requireLogin, async (req, res) => {
 
   try {
+    // 到資料庫找我們登入時依我們存在 session.userId 使用者，查找是否有書單。
     const books = await book.findAll({where:{
       memberId: req.session.userId
     },raw: true})
 
+    // 將結果回傳至畫面。
     res.render('index',{'username': req.session.user, books: books})
   } catch (error) {
-    res.render('index',{'username': req.session.user, books: error})
+    throw new Error(error)
   }
 })
 
